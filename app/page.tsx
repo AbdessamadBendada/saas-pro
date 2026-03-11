6e1cbeefe0f93e70846d2b230647b7d514670ca6
Abdessamad BENDADA <abdessamad.bendada96@gmail.com>
2026-03-11 17:22:43 +0000

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
-  return (
-    <div>Page</div>
-  )
+  return (<>
+  <h1 className='text-2xl underline'>Welcome to my SaaS app</h1> 
+    <Button className='bg-blue-500 text-white mt-4' >Get Started</Button>
+    </>
+
+)
 }
 
 export default Page
